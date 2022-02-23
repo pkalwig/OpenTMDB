34fd358cd227dbabf02dd1193dc1917073bdf24e
Piotr Kalwig <p.kalwig88@gmail.com>
2022-02-23 20:34:30 +0100

    Initial Commit

---
+++ OpenTmdb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenTmdb
+//
+//  Created by ingenico on 23/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
